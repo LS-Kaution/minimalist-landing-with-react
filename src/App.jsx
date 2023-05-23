c208c956cab8ec4755a0b70fc21dd6b0d64ea6d2
LS-Kaution <leonardosaturnoplaneta@gmail.com>
2023-05-23 13:42:56 -0400

    Added style to the first main container

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <main>
-        <section>
-          <div>
-            <img src="" alt="" />
-          </div>
+      <main className='flex justify-center items-center h-screen w-screen bg-gradient-to-tr from-white-bg to-blue-bg'>
+        <section className='flex items-center gap-20'>
+          <figure>
+            <img src="/public/img-perfil.jpg" alt="perfil image" className='h-96 w-96 rounded-full border-4 border-[#42363687] p-2 animate-rotation'/>
+          </figure>
 
           <div>
             <h1>Netherim</h1>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'white-bg': '#F2F0EB',
+        'blue-bg': '#86C0D9',
+      },
+      gradientColorStopPositions: {
+        35: '35%',
+      },
+      animation: {
+        'rotation': 'rotation 1s ease',
+      },
+      keyframes: {
+        rotation: {
+          '0%': { transform: 'rotate(-10deg)' },
+          '100%': { transform: 'rotate(0deg)' },
+        },
+      },
+    },
   },
   plugins: [],
 }
