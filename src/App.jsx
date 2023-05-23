0339800f522c66e3d3542619938d40e5d3f9d30f
LS-Kaution <leonardosaturnoplaneta@gmail.com>
2023-05-23 16:38:27 -0400

    Added style to titles h1 and h2

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,13 @@ function App() {
       <main className='flex justify-center items-center h-screen w-screen bg-gradient'>
         <section className='flex items-center gap-20'>
           <figure>
-            <img src="/public/img-perfil.jpg" alt="perfil image" className='h-96 w-96 rounded-full border-4 border-[#42363687] p-2 animate-rotation'/>
+            <img src="/public/img-perfil.jpg" alt="perfil image" className='h-96 w-96 rounded-full 
+            border-3 border-[#42363687] p-2 animate-rotation'/>
           </figure>
 
-          <div>
-            <h1>Netherim</h1>
-            <h2>Find all the links to your dragon prince here</h2>
+          <div className='flex flex-col gap-5'>
+            <h1 className='font-garamond text-[#423636] text-6xl'>Netherim</h1>
+            <h2 className='font-fraunces text-[#423636] italic text-custom'>Find all the links to your dragon prince here</h2>
           </div>
         </section>
       </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,32 @@ export default {
       backgroundImage: {
         'gradient': 'linear-gradient(45deg, #F2F0EB 54%, #86C0D9 100%)'
       },
+      fontFamily: {
+        garamond: "'EB Garamond', serif",
+        fraunces: "'Fraunces', serif"
+      },
+      fontSize: {
+        custom: '17px',
+        sm: '0.8rem',
+        base: '1rem',
+        xl: '1.25rem',
+        '2xl': '1.563rem',
+        '3xl': '1.953rem',
+        '4xl': '2.441rem',
+        '5xl': '3.052rem',
+      },
       colors: {
         'white-bg': '#F2F0EB',
         'blue-bg': '#86C0D9',
+      },
+      borderWidth: {
+        DEFAULT: '1px',
+        '0': '0',
+        '2': '2px',
+        '3': '3px',
+        '4': '4px',
+        '6': '6px',
+        '8': '8px',
       },
       animation: {
         'rotation': 'rotation 1s ease',
